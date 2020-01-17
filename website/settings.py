f4dedca9034e8b4456be133f4cd05e451630f573
Julian Espinel <julianespinel@gmail.com>
2020-01-17 22:02:24 +0200

    Add blog application

--- website/settings.py
+++ website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'about.apps.AboutConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
--- website/urls.py
+++ website/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('users/', include('django.contrib.auth.urls')),
     path('about/', include('about.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
