6eee46bc0b36ca1e50fdf38093c471d13a54f0fe
Julian Espinel <julianespinel@gmail.com>
2020-01-22 22:35:26 +0200

    Render Mardown files with Github-Flavored Markdown

--- blog/business/markdown_to_html.py
+++ blog/business/markdown_to_html.py
@@ -12,6 +12,8 @@ import glob
 import markdown
 import logging
 
+from mdx_gfm import GithubFlavoredMarkdownExtension
+
 
 logger = logging.getLogger(__name__)
 
@@ -23,8 +25,9 @@ def convert_all():
     markdown_files = __list_files_from(directory, file_extension)
     logger.info(f'listdir: {os.listdir(".")}')
     logger.info(f'markdown_files: {markdown_files}')
+    markdown_converter = markdown.Markdown(extensions=[GithubFlavoredMarkdownExtension()])
     for md_file in markdown_files:
-        __convert_single(md_file)
+        __convert_single(markdown_converter, md_file)
     logger.info('End process to convert Markdown to HTML')
 
 
@@ -32,12 +35,11 @@ def __list_files_from(directory, file_extension):
     return glob.glob(f'{directory}/*{file_extension}')
 
 
-def __convert_single(markdown_file_path):
+def __convert_single(converter, markdown_file_path):
     output_file_name = __get_file_name_only(markdown_file_path) + ".html"
     output_file_path = f'blog/templates/blog/posts/{output_file_name}'
     logger.info(f'About to write to: {output_file_path}')
-    markdown.markdownFromFile(
-        input=markdown_file_path, output=output_file_path)
+    converter.convertFile(input=markdown_file_path, output=output_file_path, encoding='utf-8')
     logger.info(f'converted {markdown_file_path} to {output_file_path}')
 
 
