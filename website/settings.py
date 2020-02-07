773cfeff7811480f6c54d1db6ca08eb77bee1205
Julian Espinel <julianespinel@gmail.com>
2020-02-07 23:51:23 +0200

    Define project base and css

--- website/settings.py
+++ website/settings.py
@@ -115,6 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/about'
