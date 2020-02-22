606de61226d9183d09c846484d2539b7a41a4858
Julian Espinel <julianespinel@gmail.com>
2020-02-22 20:39:17 +0200

    Do not convert from md to html in startup

--- blog/apps.py
+++ blog/apps.py
@@ -1,13 +1,5 @@
 from django.apps import AppConfig
-from blog.business.markdown_to_html import convert_all
-
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 class BlogConfig(AppConfig):
     name = 'blog'
-
-    def ready(self):
-        convert_all()
