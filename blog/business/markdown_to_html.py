c4fa4520f7adf8313ca57e074e29301965a12b68
Julian Espinel <julianespinel@gmail.com>
2020-02-22 21:14:11 +0200

    Bug fix: create posts html output directory

--- blog/business/markdown_to_html.py
+++ blog/business/markdown_to_html.py
@@ -5,7 +5,10 @@ and then I will create files to separate different busines logic,
 for example: `posts.py`, `comments.py`, etc.
 """
 
+from pathlib import Path
+from ..models import Post
 from django.apps import AppConfig
+from mdx_gfm import GithubFlavoredMarkdownExtension
 from django_static_image import DjangoStaticImageExtension
 
 import os
@@ -14,12 +17,10 @@ import markdown
 import logging
 import hashlib
 
-from ..models import Post
-
-from mdx_gfm import GithubFlavoredMarkdownExtension
-
 
 logger = logging.getLogger(__name__)
+
+POSTS_PATH = 'blog/templates/blog/posts/'
 
 
 def get_posts():
@@ -69,6 +70,7 @@ def convert_markdown_files():
         ]
     )
     posts = []
+    __create_posts_directory_if_needed()
     for md_file in markdown_files:
         metadata = __to_html(markdown_converter, md_file)
         post = get_post(metadata)
@@ -92,10 +94,14 @@ def __list_files_from(directory, file_extension):
     return glob.glob(f'{directory}/*{file_extension}')
 
 
+def __create_posts_directory_if_needed():
+    Path(POSTS_PATH).mkdir(parents=True, exist_ok=True)
+
+
 def __to_html(converter, markdown_file_path):
     slug = __get_file_name_only(markdown_file_path)
     output_file_name = slug + ".html"
-    output_file_path = f'blog/templates/blog/posts/{output_file_name}'
+    output_file_path = POSTS_PATH + output_file_name
     logger.info(f'About to write to: {output_file_path}')
     checksum = __get_file_checksum(markdown_file_path)
     converter.convertFile(input=markdown_file_path,
