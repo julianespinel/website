30c1707a6eb5c375bb3a1fecb013d099bb8c46fc
Julian Espinel <julianespinel@gmail.com>
2020-06-26 09:13:59 +0300

    Add command to convert markdown files to html

--- blog/apps.py
+++ blog/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'blog'
-
-    def ready(self):
-        from .business import markdown_to_html
-        markdown_to_html.refresh_posts()
--- docker/init-services.sh
+++ docker/init-services.sh
@@ -1,2 +1,3 @@
+python manage.py md_to_html --settings=settings.prod
 service nginx start
 gunicorn website.wsgi
