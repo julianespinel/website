da8d2896125699ea5bf661d6366701af3db435f1
Julian Espinel <julianespinel@gmail.com>
2020-06-28 09:44:13 +0300

    Improve description

--- blog/management/commands/md_to_html.py
+++ blog/management/commands/md_to_html.py
@@ -3,7 +3,7 @@ from blog.business import markdown_to_html
 
 
 class Command(BaseCommand):
-    help = 'Convert posts written in markdown to html'
+    help = 'Converts posts written in markdown to html'
 
     def handle(self, *args, **options):
         markdown_to_html.refresh_posts()
