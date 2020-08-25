b39ebe0a04e92f3fb2971f4c1f8034c06abf0413
Julian Espinel <julianespinel@gmail.com>
2020-08-25 19:12:29 +0300

    Fix typo

--- docker/start-docker.sh
+++ docker/start-docker.sh
@@ -1,7 +1,7 @@
 docker run -it -p 80:80 \
      -e POSTGRES_DB=<db-name> \
      -e POSTGRES_USER=<username> \
-     -e POSTGRES_PASSWORD=<passowrd> \
+     -e POSTGRES_PASSWORD=<password> \
      -e POSTGRES_HOST=<host> \
      -e POSTGRES_PORT=5432 \
      -e DJANGO_LOG_LEVEL=INFO \
