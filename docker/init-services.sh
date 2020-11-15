137cf4f3cd63441620c467b06874a6d7d2688916
Julian Espinel <julianespinel@gmail.com>
2020-11-14 19:43:46 -0500

    Create and execute migrations on startup

--- docker/init-services.sh
+++ docker/init-services.sh
@@ -1,3 +1,5 @@
+python manage.py makemigrations --settings=settings.prod
+python manage.py migrate --settings=settings.prod
 python manage.py copy_version --settings=settings.prod
 python manage.py md_to_html --settings=settings.prod
 service nginx start
