719291c813d85afa7007092973e0b29e3633c05b
Julian Espinel <julianespinel@gmail.com>
2021-10-03 12:09:25 -0400

    Add Pelican files to generate and publish content

---
+++ pelicanconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Julian Espinel'
+SITENAME = 'jespinel'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'UTC'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['sort_by_article_count']
+
+GOOGLE_ANALYTICS = ""
+WEBSITE_VERSION = "x.y.z"
