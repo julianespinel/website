db9911ebaadc825f23dde801b93ec47a5251ff94
Julian Espinel <julianespinel@gmail.com>
2021-10-03 12:02:09 -0400

    Remove code used to generate the website dynamically

--- health/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-
-class HealthTest(TestCase):
-
-    def test_website_is_healthy(self):
-        response = self.client.get(reverse('health:index'))
-        self.assertContains(response, "I'm healthy")
